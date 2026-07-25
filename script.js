5edf841d8542ca23005ec6587252f8cb0bdd8282
animehab123 <peeyushbhensale@gmail.com>
2026-07-25 20:32:11 +0530

    Initialize game canvas and player drawing

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
 
+let player = {
+  x: 180,
+  y: 500,
+  size: 30,
+  color: "blue"
+};
+
+function drawPlayer() {
+  ctx.fillStyle = player.color;
+  ctx.fillRect(player.x, player.y, player.size, player.size);
+}
+
+function gameLoop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  drawPlayer();
+
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
