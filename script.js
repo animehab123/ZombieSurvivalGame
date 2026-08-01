817853d93670e750dada27ac3b65e18b6296de9d
animehab123 <peeyushbhensale@gmail.com>
2026-08-01 15:37:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
-const canvas = document.getElementById("gameCanvas");
-const ctx = canvas.getContext("2d");
 
-let player = {
-  x: 180,
-  y: 500,
-  size: 30,
-  color: "blue"
-};
-
-function drawPlayer() {
-  ctx.fillStyle = player.color;
-  ctx.fillRect(player.x, player.y, player.size, player.size);
-}
-
-function gameLoop() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  drawPlayer();
-
-  requestAnimationFrame(gameLoop);
-}
-
-gameLoop();
 <script>
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
@@ -124,3 +101,4 @@ function gameLoop(){
 }
 
 gameLoop();
+</script>
